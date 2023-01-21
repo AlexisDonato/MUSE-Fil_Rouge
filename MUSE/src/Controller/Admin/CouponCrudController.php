27bbb4eb17582ebb002dae0c6fc6bebb0c54debf
AlexisDonato <alek6.don@gmail.com>
2023-01-21 14:16:27 +0100

    :fire:

--- MUSE/src/Controller/Admin/CouponCrudController.php
+++ MUSE/src/Controller/Admin/CouponCrudController.php
@@ -5,7 +5,9 @@ namespace App\Controller\Admin;
 use App\Entity\Coupon;
 use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class CouponCrudController extends AbstractCrudController
@@ -22,13 +24,14 @@ class CouponCrudController extends AbstractCrudController
                 ->hideOnForm();
 
         yield TextField::new('code');
-        
-        yield TextField::new('userName');
 
         yield Field::new('discountRate');
         
         yield Field::new('validated');
 
-        yield Field::new('Cart');
+        yield AssociationField::new('cart')
+        ->autocomplete()
+        ->setSortable(true);
+
     }
 }
